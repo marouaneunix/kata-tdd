9caf2c62781049239fedf8af81cf8f2bac823aa1
mar1 <elmerrouni.marouane@gmail.com>
2020-06-11 21:27:20 +0100

    initial commit

--- junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTests.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTests.java
@@ -23,19 +23,7 @@ class CalculatorTests {
 	@DisplayName("1 + 1 = 2")
 	void addsTwoNumbers() {
 		Calculator calculator = new Calculator();
-		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
-	}
 
-	@ParameterizedTest(name = "{0} + {1} = {2}")
-	@CsvSource({
-			"0,    1,   1",
-			"1,    2,   3",
-			"49,  51, 100",
-			"1,  100, 101"
-	})
-	void add(int first, int second, int expectedResult) {
-		Calculator calculator = new Calculator();
-		assertEquals(expectedResult, calculator.add(first, second),
-				() -> first + " + " + second + " should equal " + expectedResult);
+		assertEquals(calculator.add(""), 0);
 	}
 }
