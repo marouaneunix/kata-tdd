fc23f54041c39c161b464926be4091e4d291eb4b
mar1 <elmerrouni.marouane@gmail.com>
2020-06-12 12:40:26 +0100

    Active

--- junit5-jupiter-starter-maven/src/main/java/com/example/project/Calculator.java
+++ junit5-jupiter-starter-maven/src/main/java/com/example/project/Calculator.java
@@ -2,10 +2,25 @@
 
 package com.example.project;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculator {
 
-
     public int add(String s) {
-        return 0;
+        if(s.isEmpty()) {
+            return 0;
+        }
+        if(s.startsWith("//")){
+            String separator = s.substring(2,s.indexOf("\n"));
+            String c = s.substring(s.indexOf("\n"));
+            System.out.println(c);
+            return Arrays.stream("1,2,3,5".split(","))
+                    .map(Integer::parseInt)
+                    .reduce(0, Integer::sum);
+        }
+        return Arrays.stream(s.split("[,\\n]"))
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum);
     }
 }
--- junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTests.java
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTests.java
@@ -10,20 +10,62 @@
 
 package com.example.project;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CalculatorTests {
 
+	Calculator calculator;
+	@BeforeEach
+	void setup() {
+		calculator = new Calculator();
+	}
+
 	@Test
 	@DisplayName("empty = 0")
-	void addsTwoNumbers() {
-		Calculator calculator = new Calculator();
-
+	void shouldReturn0WhenEmpty() {
 		assertEquals(calculator.add(""), 0);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "1", "2", "3" })
+	void shouldReturnN(String value) {
+		assertThat(calculator.add(value)).isEqualTo(Integer.parseInt(value));
+	}
+
+	@Test
+	@DisplayName("1+1=2")
+	void shouldReturnSumValueWhenSeparatedByComa() {
+		assertThat(calculator.add("1,1")).isEqualTo(2);
+	}
+
+	@Test
+	@DisplayName("1+1+1+2=5")
+	void shouldReturnSu() {
+		assertThat(calculator.add("1,1,1,2")).isEqualTo(5);
+	}
+
+	@Test
+	@DisplayName("1+1+1+2=5")
+	void shouldReturnSdu() {
+		assertThat(calculator.add("1,1,1,2")).isEqualTo(5);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "1\n1,1,2", "1\n1,1\n2", "1\n1\n1,2" })
+	void shouldReturnSuh() {
+		assertThat(calculator.add("1\n1,1,2")).isEqualTo(5);
+	}
+
+	@Test
+	void should() {
+		assertThat(calculator.add("//,\n1,2,3,5")).isEqualTo(3);
 	}
 }
